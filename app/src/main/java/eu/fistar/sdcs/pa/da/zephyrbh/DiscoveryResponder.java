5540d52724dabd1d0ce05c145d909697f1e6729f
bratwurzt <johnny.galatikitis@gmail.com>
2015-07-07 01:53:46 +0200

    - first commit

--- app/src/main/java/eu/fistar/sdcs/pa/da/zephyrbh/DiscoveryResponder.java
+++ app/src/main/java/eu/fistar/sdcs/pa/da/zephyrbh/DiscoveryResponder.java
@@ -1,22 +1,11 @@
 /**
- * Copyright (C) 2014 Consorzio Roma Ricerche
- * All rights reserved
- *
- * This file is part of the Protocol Adapter software, available at
- * https://github.com/theIoTLab/ProtocolAdapter .
- *
- * The Protocol Adapter is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see http://opensource.org/licenses/LGPL-3.0
- *
+ * Copyright (C) 2014 Consorzio Roma Ricerche All rights reserved
+ * This file is part of the Protocol Adapter software, available at https://github.com/theIoTLab/ProtocolAdapter .
+ * The Protocol Adapter is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see http://opensource.org/licenses/LGPL-3.0
  * Contact Consorzio Roma Ricerche (protocoladapter@gmail.com)
  */
 
@@ -25,8 +14,6 @@ package eu.fistar.sdcs.pa.da.zephyrbh;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Parcelable;
-
 import eu.fistar.sdcs.pa.common.Capabilities;
 import eu.fistar.sdcs.pa.common.PAAndroidConstants;
 
@@ -36,69 +23,71 @@ import eu.fistar.sdcs.pa.common.PAAndroidConstants;
  * @author Marcello Morena
  * @author Alexandru Serbanati
  */
-public class DiscoveryResponder extends BroadcastReceiver {
+public class DiscoveryResponder extends BroadcastReceiver
+{
 
-    // Create the Capabilities object of the Device Adapter
-    public final static Capabilities CAPABILITIES = new Capabilities(
-            CapabilitiesConstants.CAP_BLACKLIST_SUPPORT,
-            CapabilitiesConstants.CAP_WHITELIST_SUPPORT,
-            CapabilitiesConstants.CAP_GUICONFIGURATION_ACTIVITY,
-            CapabilitiesConstants.CAP_GUICONFIGURATION_ACTIVITY_PACKAGE,
-            CapabilitiesConstants.CAP_DEV_CONF_TYPE,
-            CapabilitiesConstants.CAP_COMMANDS_SUPPORT,
-            CapabilitiesConstants.CAP_DETECT_DEV_SUPPORT,
-            CapabilitiesConstants.CAP_PREVIOUS_PAIRING_NEEDED,
-            CapabilitiesConstants.CAP_MONITOR_DISCONN_SUPPORT,
-            CapabilitiesConstants.CAP_FRIENDLY_NAME,
-            CapabilitiesConstants.CAP_ACTION_NAME,
-            CapabilitiesConstants.CAP_PACKAGE_NAME,
-            CapabilitiesConstants.CAP_CONNECTION_INITIATOR,
-            CapabilitiesConstants.CAP_AVAILABLE_DEVICES_SUPPORT
-    );
+  // Create the Capabilities object of the Device Adapter
+  public final static Capabilities CAPABILITIES = new Capabilities(
+      CapabilitiesConstants.CAP_BLACKLIST_SUPPORT,
+      CapabilitiesConstants.CAP_WHITELIST_SUPPORT,
+      CapabilitiesConstants.CAP_GUICONFIGURATION_ACTIVITY,
+      CapabilitiesConstants.CAP_GUICONFIGURATION_ACTIVITY_PACKAGE,
+      CapabilitiesConstants.CAP_DEV_CONF_TYPE,
+      CapabilitiesConstants.CAP_COMMANDS_SUPPORT,
+      CapabilitiesConstants.CAP_DETECT_DEV_SUPPORT,
+      CapabilitiesConstants.CAP_PREVIOUS_PAIRING_NEEDED,
+      CapabilitiesConstants.CAP_MONITOR_DISCONN_SUPPORT,
+      CapabilitiesConstants.CAP_FRIENDLY_NAME,
+      CapabilitiesConstants.CAP_ACTION_NAME,
+      CapabilitiesConstants.CAP_PACKAGE_NAME,
+      CapabilitiesConstants.CAP_CONNECTION_INITIATOR,
+      CapabilitiesConstants.CAP_AVAILABLE_DEVICES_SUPPORT
+  );
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        // Extract reply action from the received Intent
-        String replyAction = intent.getStringExtra(PAAndroidConstants.DA_DISCOVERY.BUNDLE_REPACT);
+  @Override
+  public void onReceive(Context context, Intent intent)
+  {
+    // Extract reply action from the received Intent
+    String replyAction = intent.getStringExtra(PAAndroidConstants.DA_DISCOVERY.BUNDLE_REPACT);
 
-        // Create a new Intent using the retrieved Reply Action
-        Intent replyIntent = new Intent();
-        replyIntent.setAction(replyAction);
+    // Create a new Intent using the retrieved Reply Action
+    Intent replyIntent = new Intent();
+    replyIntent.setAction(replyAction);
 
-        // Limit the intent to the package of PA
-        replyIntent.setPackage(PAAndroidConstants.PA_PACKAGE);
+    // Limit the intent to the package of PA
+    replyIntent.setPackage(PAAndroidConstants.PA_PACKAGE);
 
-        // Set the DA ID and DA Capabilities as extras in the Intent
-        replyIntent.putExtra(PAAndroidConstants.DA_DISCOVERY.BUNDLE_DAID, CapabilitiesConstants.DA_ID);
-        replyIntent.putExtra(PAAndroidConstants.DA_DISCOVERY.BUNDLE_DACAP, CAPABILITIES);
+    // Set the DA ID and DA Capabilities as extras in the Intent
+    replyIntent.putExtra(PAAndroidConstants.DA_DISCOVERY.BUNDLE_DAID, CapabilitiesConstants.DA_ID);
+    replyIntent.putExtra(PAAndroidConstants.DA_DISCOVERY.BUNDLE_DACAP, CAPABILITIES);
 
-        // Reply to the Discovery Request sent by the Protocol Adapter
-        context.sendBroadcast(replyIntent);
-    }
+    // Reply to the Discovery Request sent by the Protocol Adapter
+    context.sendBroadcast(replyIntent);
+  }
 
-    /**
-     * Class containing only the Capabilities related constants
-     */
-    static class CapabilitiesConstants {
+  /**
+   * Class containing only the Capabilities related constants
+   */
+  static class CapabilitiesConstants
+  {
 
-        // Generic Constants
-        public static final String DA_ID = "eu.fistar.sdcs.pa.da.zephyrbh";
+    // Generic Constants
+    public static final String DA_ID = "eu.fistar.sdcs.pa.da.zephyrbh";
 
-        // Capabilities constants, specific for each DA
-        public static final boolean CAP_BLACKLIST_SUPPORT = true;
-        public static final boolean CAP_WHITELIST_SUPPORT = true;
-        public static final String CAP_GUICONFIGURATION_ACTIVITY = "";
-        public static final String CAP_GUICONFIGURATION_ACTIVITY_PACKAGE = "";
-        public static final int CAP_DEV_CONF_TYPE = Capabilities.CONFIG_STARTUP_AND_RUNTIME;
-        public static final boolean CAP_COMMANDS_SUPPORT = true;
-        public static final boolean CAP_DETECT_DEV_SUPPORT = false;
-        public static final boolean CAP_PREVIOUS_PAIRING_NEEDED = true;
-        public static final boolean CAP_MONITOR_DISCONN_SUPPORT = true;
-        public static final String CAP_FRIENDLY_NAME = "Zephyr BioHarness Device Adapter";
-        public static final String CAP_ACTION_NAME = DA_ID + ".ZephyrBHDeviceAdapter";
-        public static final String CAP_PACKAGE_NAME = DA_ID;
-        public static final boolean CAP_CONNECTION_INITIATOR = true;
-        public static final boolean CAP_AVAILABLE_DEVICES_SUPPORT = true;
-
-    }
+    // Capabilities constants, specific for each DA
+    public static final boolean CAP_BLACKLIST_SUPPORT = true;
+    public static final boolean CAP_WHITELIST_SUPPORT = true;
+    public static final String CAP_GUICONFIGURATION_ACTIVITY = "";
+    public static final String CAP_GUICONFIGURATION_ACTIVITY_PACKAGE = "";
+    public static final int CAP_DEV_CONF_TYPE = Capabilities.CONFIG_STARTUP_AND_RUNTIME;
+    public static final boolean CAP_COMMANDS_SUPPORT = true;
+    public static final boolean CAP_DETECT_DEV_SUPPORT = false;
+    public static final boolean CAP_PREVIOUS_PAIRING_NEEDED = true;
+    public static final boolean CAP_MONITOR_DISCONN_SUPPORT = true;
+    public static final String CAP_FRIENDLY_NAME = "Zephyr BioHarness Device Adapter";
+    public static final String CAP_ACTION_NAME = DA_ID + ".ZephyrBHDeviceAdapter";
+    public static final String CAP_PACKAGE_NAME = DA_ID;
+    public static final boolean CAP_CONNECTION_INITIATOR = true;
+    public static final boolean CAP_AVAILABLE_DEVICES_SUPPORT = true;
+  }
 }
